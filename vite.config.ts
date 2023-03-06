f7121a57bc8b54a8fcefa12ddb7cf8a4471e7aae
JJ Chen <jiajiechen95@gmail.com>
2023-03-05 21:24:55 -0500

    Add configs for linting and formatting
    Signed-off-by: JJ Chen <jiajiechen95@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import { resolve } from 'path'
 
+// TODO: root alias & fix imports
 export default defineConfig({
+  resolve: {
+    alias: {
+      '/@/': resolve(__dirname, './src'),
+    },
+  },
   plugins: [preact()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.tsx'),
+      entry: resolve(__dirname, './src/main.tsx'),
       name: 'jsonresume-theme-coldbrew',
       fileName: 'main',
       formats: ['es'],
